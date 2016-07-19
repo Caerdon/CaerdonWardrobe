617bc479e4d984829db45c0cd1d7aaa4d76d233d
Caerdon <caerdon@caerdon.games>
2016-07-18 18:54:47 -0600

    Initial revision

---
+++ localization/deDE.lua
@@ -0,0 +1,10 @@
+local ADDON_NAME, namespace = ...
+local L = namespace.L
+local LOCALE = GetLocale()
+
+-- TODO: Just an example of how this works...
+-- Will add localized strings if users provide them
+if LOCALE == "deDE" then
+	L["BoA"] = "BoA"
+	L["BoE"] = "BoE"
+return end
