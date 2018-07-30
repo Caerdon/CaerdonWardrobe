ceb61a0a4f204e239c00f9151faf62b69a530357
Caerdon <caerdon@caerdon.games>
2018-07-29 23:56:13 -0600

    ElvUI fixes
    There was a possibility of some event ordering causing an error.
    Also (for ElvUI only), the very first time you launch the game and
    open your bags, items could be misidentified as unlearned.  I've
    made some changes that seem to help.

--- external/ElvUI/Core.ElvUI.lua
+++ external/ElvUI/Core.ElvUI.lua
@@ -1,3 +1,6 @@
+local isBagUpdateRequested = false
+local waitingOnBagUpdate = {}
+
 local ADDON_NAME, namespace = ...
 local L = namespace.L
 local Version = nil
@@ -13,6 +16,38 @@ if select(4, GetAddOnInfo(addonName)) then
 end
 
 if Version and bagsEnabled then
+	local function OnBagUpdate_Coroutine()
+	    for bag, bagData in pairs(waitingOnBagUpdate) do
+	    	for slot, slotData in pairs(bagData) do
+	    		for itemID, itemData in pairs(slotData) do
+					CaerdonWardrobe:UpdateButton(itemData.itemID, itemData.bag, itemData.slot, itemData.button, { showMogIcon = true, showBindStatus = true, showSellables = true } )
+	    		end
+	    	end
+
+			waitingOnBagUpdate[bag] = nil
+	    end
+
+		coroutine.yield()
+		waitingOnBagUpdate = {}
+	end
+
+	local function ScheduleItemUpdate(itemID, bag, slot, button)
+		local waitBag = waitingOnBagUpdate[tostring(bag)]
+		if not waitBag then
+			waitBag = {}
+			waitingOnBagUpdate[tostring(bag)] = waitBag
+		end
+
+		local waitSlot = waitBag[tostring(slot)]
+		if not waitSlot then
+			waitSlot = {}
+			waitBag[tostring(slot)] = waitSlot
+		end
+
+		waitSlot[tostring(itemID)] = { itemID = itemID, bag = bag, slot = slot, button = button }
+		isBagUpdateRequested = true
+	end
+
 	local ElvUIBags = ElvUI[1]:GetModule("Bags")
 
 	local function OnUpdateSlot(self, bagID, slotID)
@@ -32,9 +67,11 @@ if Version and bagsEnabled then
 				showSellables=true
 			}
 
-			CaerdonWardrobe:UpdateButton(itemID, bagID, slotID, button, options)
+			ScheduleItemUpdate(itemID, bagID, slotID, button)
+			-- CaerdonWardrobe:UpdateButton(itemID, bagID, slotID, button, options)
 		else
-			CaerdonWardrobe:UpdateButton(nil, bagID, slotID, button, nil)
+			CaerdonWardrobe:ClearButton(button)
+			-- CaerdonWardrobe:UpdateButton(nil, bagID, slotID, button, nil)
 		end
 		
 
@@ -44,6 +81,35 @@ if Version and bagsEnabled then
 		local handler = self[event]
 		if(handler) then
 			handler(self, ...)
+		end
+	end
+
+	local timeSinceLastBagUpdate = nil
+	local BAGUPDATE_INTERVAL = 0.3
+
+	local function OnUpdate(self, elapsed)
+		if(self.bagUpdateCoroutine) then
+			if coroutine.status(self.bagUpdateCoroutine) ~= "dead" then
+				local ok, result = coroutine.resume(self.bagUpdateCoroutine)
+				if not ok then
+					error(result)
+				end
+			else
+				self.bagUpdateCoroutine = nil
+			end
+			return
+		end
+
+		if isBagUpdateRequested then
+			isBagUpdateRequested = false
+			timeSinceLastBagUpdate = 0
+		elseif timeSinceLastBagUpdate then
+			timeSinceLastBagUpdate = timeSinceLastBagUpdate + elapsed
+		end
+
+		if( timeSinceLastBagUpdate ~= nil and (timeSinceLastBagUpdate > BAGUPDATE_INTERVAL) ) then
+			timeSinceLastBagUpdate = nil
+			self.bagUpdateCoroutine = coroutine.create(OnBagUpdate_Coroutine)
 		end
 	end
 
@@ -57,6 +123,7 @@ if Version and bagsEnabled then
 			eventFrame = CreateFrame("FRAME")
 			eventFrame:RegisterEvent "TRANSMOG_COLLECTION_UPDATED"
 			eventFrame:SetScript("OnEvent", OnEvent)
+			eventFrame:SetScript("OnUpdate", OnUpdate)
 			-- Seem to need both?  Not sure.
 			-- First one works if I do it outside OnInitialize.
 			-- Second one works in OnIntialize.
