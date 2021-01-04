cc45d88a80cccb1f052ba7238e87bd350eb4ee7d
Kkthnx <40672673+Kkthnx@users.noreply.github.com>
2021-01-03 21:28:56 -0500

    Update Core.KkthnxUI.lua

--- external/KkthnxUI/Core.KkthnxUI.lua
+++ external/KkthnxUI/Core.KkthnxUI.lua
@@ -1,26 +1,26 @@
 local ADDON_NAME, namespace = ...
 local L = namespace.L
-local Version = nil
 
+local cargBags = nil
 local addonName = "KkthnxUI"
-local KkthnxUIMixin = {}
+local CargBagsMixin = {}
 
-function KkthnxUIMixin:GetName()
-    return addonName
+function CargBagsMixin:GetName()
+	return addonName
 end
 
-function KkthnxUIMixin:Init()
-    return {"PLAYER_LOGIN"}
+function CargBagsMixin:Init()
+	return {"PLAYER_LOGIN"}
 end
 
-function KkthnxUIMixin:PLAYER_LOGIN()
-    local KkthnxUIBags = KkthnxUI[1].cargBags:GetImplementation("KKUI_Backpack")
-    if KkthnxUIBags then
-        hooksecurefunc(KkthnxUIBags, "UpdateSlot", function(...) self:UpdateSlot(...) end)
-    end
+function CargBagsMixin:PLAYER_LOGIN()
+	local impl = cargBags:GetImplementation("KKUI_Backpack")
+	if impl then
+		hooksecurefunc(impl, "UpdateSlot", function(...) self:UpdateSlot(...) end)
+	end
 end
 
-function KkthnxUIMixin:SetTooltipItem(tooltip, item, locationInfo)
+function CargBagsMixin:SetTooltipItem(tooltip, item, locationInfo)
 	if locationInfo.bag == BANK_CONTAINER then
 		local hasItem, hasCooldown, repairCost, speciesID, level, breedQuality, maxHealth, power, speed, name = tooltip:SetInventoryItem("player", BankButtonIDToInvSlotID(locationInfo.slot))
 	else
@@ -28,11 +28,11 @@ function KkthnxUIMixin:SetTooltipItem(tooltip, item, locationInfo)
 	end
 end
 
-function KkthnxUIMixin:Refresh()
+function CargBagsMixin:Refresh()
 	KkthnxUI[1]:GetModule("Bags"):UpdateAllBags()
 end
 
-function KkthnxUIMixin:UpdateSlot(frame, bagID, slotID)
+function CargBagsMixin:UpdateSlot(frame, bagID, slotID)
 	local iconSize = KkthnxUI[2].Inventory.IconSize * 0.6
 	local button = frame:GetButton(bagID, slotID)
 	local options = {
@@ -49,11 +49,21 @@ function KkthnxUIMixin:UpdateSlot(frame, bagID, slotID)
 	end
 end
 
+local Version = nil
 if select(4, GetAddOnInfo(addonName)) then
 	if IsAddOnLoaded(addonName) then
-		if KkthnxUI[2].Inventory.Enable then
-			Version = GetAddOnMetadata(addonName, 'Version')
-			CaerdonWardrobe:RegisterFeature(KkthnxUIMixin)
+		local global = _G[addonName]
+		if global then
+			cargBags = global.cargBags
+		end
+
+		Version = GetAddOnMetadata(addonName, "Version")
+		if not Version then
+			Version = "Unknown"
+		end
+
+		if cargBags then
+			CaerdonWardrobe:RegisterFeature(CargBagsMixin)
 		end
 	end
 end
