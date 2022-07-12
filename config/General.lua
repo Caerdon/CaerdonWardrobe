d88cce853653ae37133b9fb583318c885eb6109b
Caerdon <caerdon@caerdon.games>
2022-07-12 00:01:20 -0600

    Clean up and support dependent checkboxes

--- config/General.lua
+++ config/General.lua
@@ -13,8 +13,6 @@ CAERDON_GENERAL_ICON_CONFIG_POSITION = L["Icon Position:"]
 function CaerdonWardrobeConfigGeneralMixin:UpdatePendingValues()
 	local config = pendingConfig
 
-	-- config.Debug.Enabled = components.enableDebug:GetChecked()
-	-- config.Icon.EnableAnimation = components.mogIconShowAnimation:GetChecked()
 	-- config.ShowBoA = components.showBoA:GetChecked()
 	-- config.ShowBoE = components.showBoE:GetChecked()
 end
@@ -25,7 +23,9 @@ function CaerdonWardrobeConfigGeneralMixin:OnLoad()
 	self.options = {
         enableDebug = { text = "Enable Debug", tooltip = "Enables debugging info in tooltips", default = NS:GetDefaultConfig().Debug.Enabled and "1" or "0" },
         enableIconAnimation = { text = "Show Icon Animation", tooltip = "Turns icon animation on / off (largely in unlearned and openable items)", default = NS:GetDefaultConfig().Icon.EnableAnimation and "1" or "0" },
-        iconPosition = { text = "Select Icon Position", tooltip = "Configures placement of the primary collectible icon", default = "TOPLEFT" }
+        iconPosition = { text = "Select Icon Position", tooltip = "Configures placement of the primary collectible icon", default = "TOPLEFT" },
+        sameLookDifferentItem = { text = "Include different items w/ the same look (you completionist, you)", tooltip = "Ensures that you learn every single item that provides the same exact appearance for no other reason than you know you don't have that one.", default = NS:GetDefaultConfig().Icon.ShowLearnable.SameLookDifferentItem and "1" or "0" },
+        sameLookDifferentLevel = { text = "Including identical items w/ lower levels", tooltip = "Enable this to ensure that an item will show as learnable if the item's level would allow a lower level toon to use it for transmog than the one you already know.", default = NS:GetDefaultConfig().Icon.ShowLearnable.SameLookDifferentLevel and "1" or "0" }
 	}
 
 	InterfaceOptionsPanel_OnLoad(self);
@@ -34,6 +34,13 @@ end
 function CaerdonWardrobeConfigGeneralMixin:ConfigureCheckbox(checkbox, label, configSection, configSubsection, configValue)
     checkbox.type = CONTROLTYPE_CHECKBOX;
     checkbox.label = label;
+    checkbox.Text:SetWidth(230)
+
+    local dependsOn, dependsOnControl
+    if checkbox.dependentOn then
+        dependsOn = checkbox.dependentOn:gsub( "$parent", checkbox:GetParent():GetName())
+        dependsOnControl = _G[dependsOn]
+    end
 
     checkbox.GetValue = function (self)
         local section = configSection and configSubsection and CaerdonWardrobeConfig[configSection][configSubsection] or configSection and CaerdonWardrobeConfig[configSection]
@@ -45,21 +52,39 @@ function CaerdonWardrobeConfigGeneralMixin:ConfigureCheckbox(checkbox, label, co
     end
 
     BlizzardOptionsPanel_RegisterControl(checkbox, self);
+    if dependsOnControl then
+        BlizzardOptionsPanel_SetupDependentControl(dependsOnControl, checkbox)
+    end
 end
 
-function CaerdonWardrobeConfigGeneralMixin:ConfigureDropdown(dropdown, label, configSection, configSubsection, configValue)
+function CaerdonWardrobeConfigGeneralMixin:ConfigureDropdown(dropdown, label, configSection, configSubsection, configValue, items)
     dropdown:SetScript("OnEvent", function (self, event, ...)
         if ( event == "PLAYER_ENTERING_WORLD" ) then
-            self:GetParent():InitializeDropdown(dropdown, label, configSection, configSubsection, configValue)
+            self:GetParent():InitializeDropdown(dropdown, label, configSection, configSubsection, configValue, items)
             self:UnregisterEvent(event)
         end
     end)
+
+    dropdown:SetScript("OnEnter", function (self)
+        if ( not self.isDisabled ) then
+            GameTooltip:SetOwner(self, "ANCHOR_TOPRIGHT");
+            GameTooltip:SetText(self.tooltipText, nil, nil, nil, nil, true);
+        end
+    end)
+
+    dropdown:SetScript("OnLeave", function (self)
+        if ( GameTooltip:GetOwner() == self ) then
+            GameTooltip:Hide();
+        end
+    end)
+
     dropdown:RegisterEvent("PLAYER_ENTERING_WORLD");
 end
 
-function CaerdonWardrobeConfigGeneralMixin:InitializeDropdown(dropdown, label, configSection, configSubsection, configValue)
+function CaerdonWardrobeConfigGeneralMixin:InitializeDropdown(dropdown, label, configSection, configSubsection, configValue, items)
     dropdown.type = CONTROLTYPE_DROPDOWN;
     dropdown.label = label
+    dropdown.items = items
 
     dropdown.defaultValue = self.options[label].default
     local section = configSection and configSubsection and CaerdonWardrobeConfig[configSection][configSubsection] or configSection and CaerdonWardrobeConfig[configSection]
@@ -79,21 +104,18 @@ function CaerdonWardrobeConfigGeneralMixin:InitializeDropdown(dropdown, label, c
     dropdown.RefreshValue =
         function (self)
             UIDropDownMenu_SetSelectedValue(self, self.value);
-            UIDropDownMenu_Initialize(self, self:GetParent().AddSelections);
+            UIDropDownMenu_Initialize(self, function ()
+                for itemIndex = 1, #self.items do
+                    local item = self.items[itemIndex]
+                    self:GetParent():AddDropDownItem(self, item.title, item.value, item.tooltip)
+                end
+            end);
         end
 
     BlizzardOptionsPanel_RegisterControl(dropdown, self);
 
     UIDropDownMenu_SetWidth(dropdown, 136);
-    UIDropDownMenu_SetSelectedValue(dropdown, dropdown.value);
-    UIDropDownMenu_Initialize(dropdown, self.AddSelections);
-end
-
-function CaerdonWardrobeConfigGeneralMixin:AddSelections()
-	self:GetParent():AddDropDownItem(self, "Top Left", "TOPLEFT", "Show the primary icon in the top left")
-	self:GetParent():AddDropDownItem(self, "Top Right", "TOPRIGHT", "Show the primary icon in the top right")
-	self:GetParent():AddDropDownItem(self, "Bottom Left", "BOTTOMLEFT", "Show the primary icon in the bottom left")
-	self:GetParent():AddDropDownItem(self, "Bottom Right", "BOTTOMRIGHT", "Show the primary icon in the bottom right")
+    dropdown:RefreshValue()
 end
 
 function CaerdonWardrobeConfigGeneralMixin:AddDropDownItem(dropdown, name, value, tooltip)
