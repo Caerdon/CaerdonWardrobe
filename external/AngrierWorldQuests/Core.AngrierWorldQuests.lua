cd900fe943023b20bc1b2e040e7a4d1a1e203e8e
Caerdon <caerdon@caerdon.games>
2022-11-12 10:09:43 -0700

    Add support for AngrierWorldQuests

--- external/AngrierWorldQuests/Core.AngrierWorldQuests.lua
+++ external/AngrierWorldQuests/Core.AngrierWorldQuests.lua
@@ -2,7 +2,6 @@ local ADDON_NAME, namespace = ...
 local L = namespace.L
 
 local addonName = "AngrierWorldQuests"
--- local AngrierWorldQuests = AngrierWorldQuestsAddon
 local AngrierWorldQuestsMixin = {}
 
 function AngrierWorldQuestsMixin:GetName()
@@ -12,17 +11,15 @@ end
 local isHooked = false
 
 function AngrierWorldQuestsMixin:Init()
-	-- TODO: Disabling for now until I can figure out how to enumerate the acquired frames successfully.
+	local addon = _G[addonName]
+	local questFrame = addon.Modules["QuestFrame"]
 
-	-- local addon = _G[addonName]
-	-- local questFrame = addon.Modules["QuestFrame"]
-
-	-- hooksecurefunc(questFrame, "Startup", function(...) 
-	-- 	if not isHooked then
-	-- 		isHooked = true
-	-- 		hooksecurefunc("QuestLogQuests_Update", function(...) self:OnQuestLogQuests_Update(...) end)
-	-- 	end
-	-- end)
+	hooksecurefunc(questFrame, "Startup", function(...) 
+		if not isHooked then
+			isHooked = true
+			hooksecurefunc("QuestLogQuests_Update", function(...) self:OnQuestLogQuests_Update(...) end)
+		end
+	end)
 end
 
 function AngrierWorldQuestsMixin:GetTooltipData(item, locationInfo)
@@ -33,19 +30,26 @@ function AngrierWorldQuestsMixin:Refresh()
 end
 
 function AngrierWorldQuestsMixin:IsSameItem(button, item, locationInfo)
-	-- TODO: Review (if I get this module working)
-	-- QuestId will be directly on world map pin but is on the grandparent for the quest log
-	local isSame = button.questId == locationInfo.questID or button:GetParent():GetParent().questId == locationInfo.questID
+	local isSame = button.questID == locationInfo.questID
 	return isSame
 end
 
 function AngrierWorldQuestsMixin:GetDisplayInfo(button, item, feature, locationInfo, options, mogStatus, bindingStatus)
 	return {
+		bindingStatus = {
+			shouldShow = true
+		},
+		ownIcon = {
+			shouldShow = true
+		},
+		otherIcon = {
+			shouldShow = true
+		},
 		questIcon = {
 			shouldShow = true
 		},
 		oldExpansionIcon = {
-			shouldShow = false
+			shouldShow = true
 		},
         sellableIcon = {
             shouldShow = false
@@ -54,15 +58,26 @@ function AngrierWorldQuestsMixin:GetDisplayInfo(button, item, feature, locationI
 end
 
 function AngrierWorldQuestsMixin:OnQuestLogQuests_Update()
-	local headerButton = AngrierWorldQuestsHeader
-	local pool = headerButton.titleFramePool
+	if self.questTimer then
+		self.questTimer:Cancel()
+	end
 
-	-- TODO: Figure out how to make this work.  GetNumActive will return a large number of items but EnumerateActive won't iterate through them all.
-	-- print("NUM ACTIVE: " .. tostring(pool:GetNumActive()))
-	for button in pool:EnumerateActive() do
-		if button then
+	self.questTimer = C_Timer.NewTimer(0.2, function ()
+		local headerButton = AngrierWorldQuestsHeader
+		if not headerButton then return end
+	
+		local pool = headerButton.titleFramePool
+
+		local count = 1
+		for button in pool:EnumerateActive() do
+			local options = {
+				relativeFrame = button.TagTexture,
+				statusProminentSize = 13,
+				statusOffsetX = 3,
+				statusOffsetY = 3
+			}
+
 			local title, factionID, capped = C_TaskQuest.GetQuestInfoByQuestID(button.questID)
-			-- print("Active: " ..title)
 
 			local questID = button.questID
 			local questLink = GetQuestLink(questID)
@@ -76,30 +91,27 @@ function AngrierWorldQuestsMixin:OnQuestLogQuests_Update()
 			local itemData = questItem:GetItemData()
 			if not itemData then
 				CaerdonWardrobe:ClearButton(button)
-				return
+			else
+				local bestIndex, bestType = QuestUtils_GetBestQualityItemRewardIndex(questID)
+
+				if not bestType then 
+					CaerdonWardrobe:ClearButton(button)
+				else
+					local itemLink = GetQuestLogItemLink(bestType, bestIndex, questID)
+					if not itemLink then
+						CaerdonWardrobe:ClearButton(button)
+					else
+						local item = CaerdonItem:CreateFromItemLink(itemLink)
+						CaerdonWardrobe:UpdateButton(button, item, self, { 
+							locationKey = format("button%dquest%d", count, questID),
+							questID = questID,
+							questItem
+						}, options)
+					end
+				end
 			end
-
-			local bestIndex, bestType = QuestUtils_GetBestQualityItemRewardIndex(questID)
-
-			if not bestType then 
-				CaerdonWardrobe:ClearButton(button)
-				return
-			end
-
-			local itemLink = GetQuestLogItemLink(bestType, bestIndex, questID)
-			if not itemLink then
-				CaerdonWardrobe:ClearButton(button)
-				return
-			end
-
-			local item = CaerdonItem:CreateFromItemLink(itemLink)
-			CaerdonWardrobe:UpdateButton(button, item, self, { 
-				locationKey = format("%d", questID),
-				questID = questID,
-				questItem
-			}, options)
 		end
-	end
+	end, 1)
 end
 
 local Version = nil
