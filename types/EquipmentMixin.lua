40206efc0783e075bd584570739aaabfc15ae4e2
Caerdon <caerdon@caerdon.games>
2024-07-01 17:49:03 -0600

    Still looking into upgrade checks

--- types/EquipmentMixin.lua
+++ types/EquipmentMixin.lua
@@ -443,14 +443,15 @@ function CaerdonEquipmentMixin:GetTransmogInfo()
     
     -- TODO: Add check if upgrading item will result in an unlearned appearance.
     -- if item:GetItemID() == 199446 then
-    local itemLocation = item:GetItemLocation()
-    if itemLocation and C_ItemUpgrade.CanUpgradeItem(itemLocation) then
-        C_ItemUpgrade.SetItemUpgradeFromLocation(item:GetItemLocation())
-        local upgradeInfo = C_ItemUpgrade.GetItemUpgradeItemInfo()
-        if upgradeInfo and upgradeInfo.currUpgrade < upgradeInfo.maxUpgrade then
-            print("Item can be upgraded: " .. item:GetItemLink() .. " to " .. upgradeInfo.upgradedItemLink)
-        end
-    end
+    -- local itemLocation = item:GetItemLocation()
+    -- if itemLocation and C_ItemUpgrade.CanUpgradeItem(itemLocation) then
+    --     C_ItemUpgrade.SetItemUpgradeFromLocation(item:GetItemLocation())
+    --     local upgradeInfo = C_ItemUpgrade.GetItemUpgradeItemInfo()
+    --     if upgradeInfo and upgradeInfo.currUpgrade < upgradeInfo.maxUpgrade then
+    --         -- print("Item can be upgraded: " .. item:GetItemLink() .. " to " .. upgradeInfo.upgradedItemLink)
+    --         DevTools_Dump(upgradeInfo)
+    --     end
+    -- end
 
     if PawnShouldItemLinkHaveUpgradeArrow then
         isUpgrade = PawnShouldItemLinkHaveUpgradeArrow(item:GetItemLink(), false)
