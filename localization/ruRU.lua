3f67100324beb3a6d4c61fcf9715d654ed2937df
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-10-24 08:00:18 +0300

    Update ruRU.lua

--- localization/ruRU.lua
+++ localization/ruRU.lua
@@ -1,11 +1,9 @@
 local ADDON_NAME, namespace = ...
 local L = namespace.L
 local LOCALE = GetLocale()
-
--- TODO: Just an example of how this works...
--- Will add localized strings if users provide them
+--Translator ZamestoTV
 if LOCALE == "ruRU" then
-	L["BoA"] = "Привязано к учетной записи"
-	L["BoE"] = "Привязано при надевании"
-	L["Equip:"] = "При надевании:"
+	L["BoA"] = "BoA"
+	L["BoE"] = "BoE"
+	L["Equip:"] = "При над.:"
 end
